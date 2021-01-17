14b73aa1170d80acb9e919c8f8e216d3b34fef1b
John Akhilomen <jonbasanti@gmail.com>
2021-01-16 23:01:13 -0600

    msg

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports= {
     API_ENDPOINT: 'https://aipnode.herokuapp.com/api',
     API_KEY: process.env.REACT_APP_API_KEY, 
     BASE_URL: "https://aipnode.herokuapp.com/api",
-    BASE_URL_FRONTEND: "https://aipreactapp.herokuapp.com/"   
+    BASE_URL_FRONTEND: "https://aipreactapp.herokuapp.com"   
   }
